c1137058399fcc9ebf7234b5c1d246491bc19ef7
naomi789 <snjohnson789@gmail.com>
2018-07-06 21:52:06 -0700

    refactoring code for less duplication

--- confidence.py
+++ confidence.py
@@ -1,32 +1,36 @@
 from overall_survey import run_overall
-from data_structures import confident_person
+from data_structures import first_confident_person, middle_confidence_person, last_confidence_person
 
 def main():
     all_students_whole_semester = import_data()
 
 
-def import_data():
-    # first survey # raw_first_confidence_survey/confidence_preliminary.csv
-    data = run_overall('raw_first_confidence_survey/confidence_preliminary.csv')
+def one_source(file, tuple_results):
+    # first survey  # raw_first_confidence_survey/confidence_preliminary.csv
+    data = run_overall(file)
     data = data[2:]  # deletes the question text and shorthand from the dataset
 
-    all_raw_preliminary_students = list(map(lambda line: confident_person(*line), data))
-    preliminary_students = []
-    for person in all_raw_preliminary_students:  # filters out all responses where there is no gender
+    processed = list(map(lambda line: tuple_results(*line), data))
+    answer = []
+    for person in processed:  # filters out all responses where there is no gender
         if getattr(person, 'gender') is not '':
-            preliminary_students.append(person)
+            answer.append(person)
 
+    return answer
+
+def import_data():
+    # first survey # raw_first_confidence_survey/confidence_preliminary.csv
+    first_survey = one_source('raw_first_confidence_survey/confidence_preliminary.csv', first_confident_person)
 
     # middle surveys # raw_middle_confidence_survery/raw_middle.csv
+    # for file in folder:
+    middle_survey = one_source('raw_middle_confidence_survery/raw_middle.csv', middle_confidence_person)
+
+    # final_surveys # raw_final_confidence_survey/final_raw.csv
+    final_survey = one_source('raw_final_confidence_survey/confidence_final.csv', last_confidence_person)
 
 
-    # final_sruveys # raw_final_confidence_survey/final_raw.csv
 
-    final_data = run_overall('raw_final_confidence_survey/confidence_final.csv')
-    data = data[:2]
-    all_raw_final_students = [] # list(map(lambda line: confident_person(*line), data))
-    final_students = []
 
     all_students_whole_semester = []
-
     return all_students_whole_semester
--- data_structures.py
+++ data_structures.py
@@ -1,7 +1,20 @@
 from collections import namedtuple
 
-confidence_survey = ['email','gender', 'gender_other', 'gpa', 'major', 'graduating_year', 'confidence_prepared', 'confidence_ability', 'confidence_general_classes', 'confidence_major', 'confidence_get_degree', 'confidence_percentile']
+first_confidence_survey = ['email', 'gender', 'gender_other', 'gpa', 'major', 'graduating_year', 'confidence_prepared',
+                           'confidence_ability', 'confidence_general_classes', 'confidence_major',
+                           'confidence_get_degree', 'confidence_percentile']
 
-confident_person = namedtuple('Confident_person', confidence_survey)
+first_confident_person = namedtuple('Confident_person', first_confidence_survey)
+
+middle_confidence_survey = ['identifier', 'confidence_general_classes', 'confidence_major', 'confidence_get_degree',
+                            'confidence_percentile']
+
+middle_confidence_person = namedtuple('middle_confident_person', middle_confidence_survey)
+
+last_confidence_survey = ['identifier', 'confidence_general_classes', 'confidence_major', 'confidence_get_degree',
+                          'confidence_get_degree', 'confidence_career', 'confidence_percentile', '1_class', '1_grade',
+                          '2_class', '2_grade', '3_class', '3_grade', '4_class', '4_grade']
+
+last_confidence_person = namedtuple('last_confidence_person', last_confidence_survey)
 
 full_semester_of_data = []
