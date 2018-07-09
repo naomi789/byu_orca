682f6335bba637e91444bc40b1a7041de33633d5
naomi789 <snjohnson789@gmail.com>
2018-07-08 20:41:07 -0700

    parsing other confidence surveys

--- confidence.py
+++ confidence.py
@@ -1,24 +1,11 @@
 from overall_survey import run_overall
 from data_structures import first_confident_person, middle_confidence_person, last_confidence_person
 
+
 def main():
     first_survey, all_middle_results, final_survey = import_data()
+    unite_into_people(first_survey, all_middle_results, final_survey)
 
-    # graph all confidence points across time, one line per CLASS (expect: see differences) (do I care about professor differences? no?)
-    # graph all confidence points across time, one line per GENDER (expect: women are lower than men)
-    # graph all confidence points across time, one line per FINAL GRADE (positive slope if good grade, zero/negative/shaky for bad grades)
-    # graph all confidence points across time, one line per FINAL GRADE PER GENDER (women need a higher grade to see an uptick in confidence)
-    # graph all confidence points across time, one line per class per gender (women dislike 142 and 235 more)
-    # graph all confidence points across time (expect: if confidence fell more than X points, they are no longer CS majors)
-    # compare overall GPA to confidence levels (each metric) (expect: correlation. ofc.)
-    # compare overall GPA to confidence levels (each metric) PER GENDER (expect: given X gpa, women have lower confidence than men)
-    # compare each confidence metric to the other (what is/is not consistent)
-
-    # expect: non majors who grow in confidence are more likely to switch to CS
-    # expect: CS majors who's confidence falls are more likely to leave CS
-    # expect: if I asked about age, expect younger students to have weaker study habits --> less confidence EXCEPT UW paper questions
-    # expect: sooner graduation date --> more confidence in intro CS courses? or less bc they've been nurveous and procrastinating?
-    # expect: upperclassmen to feel more prepared before the semester. Have more confidence in graduating. )
 
 def one_source(file, tuple_for_results):
     # first survey  # raw_first_confidence_survey/confidence_preliminary.csv
@@ -26,7 +13,7 @@ def one_source(file, tuple_for_results):
     data = data[2:]  # deletes the question text and shorthand from the dataset
 
     processed = list(map(lambda line: tuple_for_results(*line), data))
-    answer = []  # make this into a dictionary
+    answer = []  # TODO: make this into a dictionary; map + key
     for person in processed:  #  filters out all responses where there is no gender
         if getattr(person, 'gender') is not '':
             answer.append(person)
@@ -48,3 +35,15 @@ def import_data():
     final_survey = one_source('raw_final_confidence_survey/confidence_final.csv', last_confidence_person)
 
     return first_survey, all_middle_results, final_survey
+
+
+def unite_into_people(first_survey, all_middle_results, final_survey):
+    for person_id in first_survey.keys():
+        getattr(first_survey[person_id], 'identifier')
+        getattr(all_middle_results[person_id], 'identifier')  # TODO: people filled this out like seven times. Hopefully.
+        getattr(last_confidence_person[person_id], 'identifier') # TODO: what about the flakes who didn't fill out more?
+        # TODO: ideally, we can connect this to the original survey as well
+    pass
+
+
+main()
--- data_structures.py
+++ data_structures.py
@@ -1,20 +1,19 @@
 from collections import namedtuple
 
-first_confidence_survey = ['email', 'gender', 'gender_other', 'gpa', 'major', 'graduating_year', 'confidence_prepared',
+first_confidence_survey = ['date', 'identifier', 'gender', 'gender_other', 'gpa', 'major', 'graduating_year', 'confidence_prepared',
                            'confidence_ability', 'confidence_general_classes', 'confidence_major',
                            'confidence_get_degree', 'confidence_percentile']
 
 first_confident_person = namedtuple('Confident_person', first_confidence_survey)
 
-middle_confidence_survey = ['identifier', 'confidence_general_classes', 'confidence_major', 'confidence_get_degree',
-                            'confidence_percentile']
+middle_confidence_survey = ['date', 'identifier', 'confidence_general_classes', 'confidence_major_classes',
+                            'confidence_excel_graduate', 'confidence_get_degree', 'confidence_percentile']
 
 middle_confidence_person = namedtuple('middle_confident_person', middle_confidence_survey)
 
-last_confidence_survey = ['identifier', 'confidence_general_classes', 'confidence_major', 'confidence_get_degree',
-                          'confidence_get_degree', 'confidence_career', 'confidence_percentile', '1_class', '1_grade',
-                          '2_class', '2_grade', '3_class', '3_grade', '4_class', '4_grade']
+last_confidence_survey = ['date', 'identifier', 'confidence_general_classes', 'confidence_major_classes', 'confidence_excel_graduate',
+                          'confidence_get_degree', 'confidence_career', 'confidence_percentile', '1_grade', '1_class',
+                          '2_grade', '2_class', '3_grade', '3_class', '4_grade', '4_class']
 
 last_confidence_person = namedtuple('last_confidence_person', last_confidence_survey)
 
-full_semester_of_data = []
