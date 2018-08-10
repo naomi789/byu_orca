79009c1ff237f52b2e6c56189170961d76676523
naomi789 <snjohnson789@gmail.com>
2018-08-10 15:38:59 -0700

    bar graphs now work

--- naomi_plotting.py
+++ naomi_plotting.py
@@ -30,8 +30,7 @@ def get_question_df(df, question, keep_cols, get_answers=True):
             question_df[answer] = 0
             print(answer)
 
-    print({col: col.strip() for col in question_df.columns})
-    question_df.rename(columns={col: col.strip() for col in question_df.columns},
+        question_df.rename(columns={col: col.strip() for col in question_df.columns},
                        inplace=True)
 
     return question_df, list(answers_set)
@@ -41,23 +40,48 @@ def graph_data(df):
     # this does all the '% of focus_var gave this answer'
     # in other words, these will all be bar graphs
     for var in FOCUS_VARS:
+        print('var', var)
         keep_cols = [var,  GENDER]
 
         for question in TEMP_MANY_CHOICES:
+            print('question', question)
             question_df, answers = get_question_df(df, question, keep_cols)
 
-            if question == MAJOR_PROS:
-                global THING
-                THING = question_df
-                global ANSWERS
-                ANSWERS = answers
+            global DF
+            DF = question_df
+            global ANSWERS
+            ANSWERS = answers
+            global agg
 
 
-            agg = question_df[keep_cols + answers].groupby(keep_cols).aggregate(sum)
+            
+
+            if var == MAJOR:
+                print(var)
+                # question_df['Computer Science'] = question_df[MAJOR]=='Computer Science'
+                question_df['binary_CS'] = ['CS' if x else 'non_CS' for x in question_df[MAJOR]=='Computer Science']
+
+                #question_df['non_CS'] = question_df['Computer Science'].replace({1:0, 0:1})
+                DF = question_df
+                agg = question_df[keep_cols + ['binary_CS'] + answers].groupby([GENDER, 'binary_CS']).aggregate(sum)
+
+            
+            else: 
+                agg = question_df[keep_cols + answers].groupby(keep_cols).aggregate(sum)
+
+
+            gender_options = ['Male', 'Female']
+            CS = ['CS', 'non_CS']
+
             agg_t = agg.transpose()
-            (agg_t / (agg_t.sum(axis=0))).plot.bar(stacked=False)
-            plt.savefig(f'panda_BYU/{question}.png')
+            cat1 = agg_t.columns.levels[0]
+            cat2 = agg_t.columns.levels[1]
+            counts = np.array([len(DF[(DF[GENDER]==x) & (DF['binary_CS']==y)]) for x in cat1 for y in cat2])
 
+            (agg_t / (counts)).plot.bar(stacked=False)
+            plt.title(question)
+            plt.savefig(f'panda_BYU/{question}.png')
+            # {var}/
             question_df.to_csv('practice_df.csv')
 
     # these will all be STACKED bar graphs
@@ -66,7 +90,7 @@ def graph_data(df):
 
 df = pd.read_csv('raw_overall_survey/byu_for_pandas_strings_stephen_fixed.csv')
 df.dropna(subset=[RACE, GENDER, PROGRAM, MAJOR, GRAD_YEAR], inplace=True)  # tosses if participants didn't answer these
-
+df = df[(df[GENDER] == 'Male') | (df[GENDER] == 'Female')]
 graph_data(df)
 
 
