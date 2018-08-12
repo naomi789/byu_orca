4fa0e360ef649bd09899cf16fb6f7ccc46262562
naomi789 <snjohnson789@gmail.com>
2018-08-12 10:04:33 -0600

    tweaking code to make it more generalizable

--- naomi_plotting.py
+++ naomi_plotting.py
@@ -4,8 +4,10 @@ import numpy as np
 import os
 import sys
 from new_constants import *
-# from data_structures import question_number_to_expected_answer
-
+from language_processing import *
+from graph_func import pie_chart
+from answer_type import compare_confidence_GPA
+from overall_survey import response_rate, calculate_one_chart
 
 def get_question_df(df, question, keep_cols, get_answers=True):
     # find expected answer types
@@ -34,9 +36,17 @@ def get_question_df(df, question, keep_cols, get_answers=True):
     return question_df, list(answers_set)
 
 
-def many_option_graphing(df):
-    # this does all the '% of focus_var gave this answer'
-    # in other words, these will all be bar graphs
+
+def sorted_answers(question_options):
+    answer = answer_dict_switcher.get(question_options, 'error')
+    if answer == 'error':
+        raise Exception('answer type is unknown in sorted_answers()\'s switcher')
+    return answer
+
+
+
+def many_option_graphing(df): # this does all the '% of focus_var gave this answer # all be bar graphs
+    gender_options = ['Male', 'Female']
     for var in FOCUS_VARS:
         print('var', var)
         keep_cols = [var,  GENDER]
@@ -50,36 +60,95 @@ def many_option_graphing(df):
             if var == MAJOR:
                 question_df['binary_CS'] = ['CS' if x else 'non_CS' for x in question_df[MAJOR]=='Computer Science']
                 agg = question_df[keep_cols + ['binary_CS'] + answers].groupby([GENDER, 'binary_CS']).aggregate(sum)
+                columns_for_var = 'binary_CS'
             else:
+                expected_answer_types = ques_ans[question]
+                possible_answers = sorted_answers(expected_answer_types)
                 agg = question_df[keep_cols + answers].groupby(keep_cols).aggregate(sum)
-
-            gender_options = ['Male', 'Female']
-            CS = ['CS', 'non_CS']
+                columns_for_var = 'idk' # TODO
 
             agg_t = agg.transpose()
+            # CS = ['CS', 'non_CS']
             cat1 = agg_t.columns.levels[0]
             cat2 = agg_t.columns.levels[1]
-            counts = np.array([len(DF[(DF[GENDER]==x) & (DF['binary_CS']==y)]) for x in cat1 for y in cat2])
+            counts = np.array([len(DF[(DF[GENDER] == x) & (DF[columns_for_var] == y)]) for x in cat1 for y in cat2])
+
 
             ax = (agg_t / (counts)).plot(kind='barh', stacked=False)
 
-            labels = [x[:30] for x in agg_t.index]
+            labels = [x[:40] for x in agg_t.index]
             ax.set_yticklabels(labels)
 
             plt.title(question)
             plt.tight_layout()
+            plt.savefig(f'panda_BYU_results/{ques_num_to_shorthand[var]}/{ques_num_to_shorthand[question]}.png')
 
-            plt.savefig(f'panda_BYU_results/{var}/{question}.png')
-            # {var}/
-            question_df.to_csv('practice_df.csv')
 
 def one_option_graph():
     # these will all be STACKED bar graphs
     # for question in ONE_CHOICE_QUESTIONS:
-    pass
+    for var in FOCUS_VARS:
+        print('var', var)
+        keep_cols = [var,  GENDER]
+
+        for question in MANY_CHOICES_QUESTIONS:
+            print('question', question)
+            question_df, answers = get_question_df(df, question, keep_cols)
+            DF = question_df
+
+def prep_for_pie(df, attribute):
+    choice_to_answer = {}
+    for person in people:
+        this_var = getattr(person, attribute)
+        if not choice_to_answer.keys().__contains__(this_var):
+            choice_to_answer[this_var] = 0
+        else:
+            choice_to_answer[this_var] += 1
+    return choice_to_answer
 
 
-df = pd.read_csv('raw_overall_survey/byu_for_pandas_strings_stephen_fixed.csv')
-df.dropna(subset=[RACE, GENDER, PROGRAM, MAJOR, GRAD_YEAR], inplace=True)  # tosses if participants didn't answer these
-df = df[(df[GENDER] == 'Male') | (df[GENDER] == 'Female')]
-many_option_graphing(df)
+def assorted_special_graphs(df, people):
+    compare_confidence_GPA(people, 'gender')
+
+    for type_of_feedback in ['describe_positive_experience', 'describe_negative_experience']:
+        associate_with_professors(people, type_of_feedback)
+
+    # pie chart of races
+    var = 'race'
+    choice_to_answer = prep_for_pie(people, var)
+    assert choice_to_answer
+    pie_chart(var, choice_to_answer)
+
+    # pie chart of what non-CS majors there were
+    var = 'university_major'
+    choice_to_answer = prep_for_pie(people, var)
+    assert choice_to_answer
+    pie_chart(var, choice_to_answer)
+
+    # pie chart of what genders there were
+    var = 'gender'
+    choice_to_answer = prep_for_pie(people, var)
+    assert choice_to_answer
+    pie_chart(var, choice_to_answer)
+
+    # print out the stats of who responded v. who was invited to take the survey
+    response_rate(people)
+
+    # see how common different words are in free response
+    find_common_words(people)
+
+
+
+
+
+
+
+def main():
+    df = pd.read_csv('raw_overall_survey/byu_for_pandas_strings_stephen_fixed.csv')
+    df.dropna(subset=[RACE, GENDER, PROGRAM, MAJOR, GRAD_YEAR], inplace=True)  # tosses if participants didn't answer these
+    df = df[(df[GENDER] == 'Male') | (df[GENDER] == 'Female')]
+    many_option_graphing(df)
+    # assorted_special_graphs(df)
+
+
+main()
