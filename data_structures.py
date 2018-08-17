8e672240e1b1fea3466cad679a53586fe13039fa
naomi789 <snjohnson789@gmail.com>
2018-08-17 09:38:54 -0600

    removed confidence survey stuff

--- data_structures.py
+++ data_structures.py
@@ -1,22 +1,3 @@
-from collections import namedtuple
-
-first_confidence_survey = ['date', 'identifier', 'gender', 'gender_other', 'gpa', 'major', 'graduating_year',
-                           'confidence_prepared',
-                           'confidence_ability', 'confidence_general_classes', 'confidence_major',
-                           'confidence_get_degree', 'confidence_percentile']
-
-first_confident_person = namedtuple('Confident_person', first_confidence_survey)
-
-middle_confidence_survey = ['date', 'identifier', 'confidence_general_classes', 'confidence_major_classes',
-                            'confidence_excel_graduate', 'confidence_get_degree', 'confidence_percentile']
-
-middle_confidence_person = namedtuple('middle_confident_person', middle_confidence_survey)
-#
-# last_confidence_survey = ['date', 'identifier', 'confidence_general_classes', 'confidence_major_classes', 'confidence_excel_graduate',
-#                           'confidence_get_degree', 'confidence_career', 'confidence_percentile', '1_grade', '1_class',
-#                           '2_grade', '2_class', '3_grade', '3_class', '4_grade', '4_class']
-
-# last_confidence_person = namedtuple('last_confidence_person', last_confidence_survey)
 
 ques_ans = {'duration_seconds': 'int', 'Location_Latitude': 'double', 'LocationLongitude': 'double',
             'consent_current': 'string', 'consent_future': 'string', 'email': 'string', 'gender': 'string',
