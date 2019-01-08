af974c04802ad67b56eb2351528975d63285a635
naomi789 <snjohnson789@gmail.com>
2019-01-07 21:12:36 -0500

    looking back over last week's notes

--- text_analysis/text_analysis.R
+++ text_analysis/text_analysis.R
@@ -7,7 +7,7 @@ library(dplyr) # for heatmap
 library(tm) # for correlation matrix
 
 # read in data
-positive_data <- read.csv("./positive.csv", header = FALSE, stringsAsFactors = FALSE)
+positive_data <- read.csv("./text_analysis/positive.csv", header = FALSE, stringsAsFactors = FALSE)
 # /most_important_change.csv
 # /negative.csv
 # /positive.csv
@@ -20,15 +20,27 @@ corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, removeNumbers)
 corpus <- tm_map(corpus, removeWords, stopwords("english"))
 corpus <- tm_map(corpus, stemDocument)
-dtm <- DocumentTermMatrix(corpus)
-
 dtm.positive <- DocumentTermMatrix(corpus)
+
 dtm.positive <- removeSparseTerms(dtm.positive, .90)
-# dtm.positive
 dtm.positive.df <- as.data.frame(as.matrix(dtm.positive))
 
 dtm.positive.df <- sort(colSums(dtm.positive.df),decreasing=TRUE) # sorts most commonly occuring words to the top
-dtm.positive.df.freq.data <- data.frame(word = names(dtm.positive.df),freq=dtm.positive.df) # shows them as a DF
+head(dtm.positive.df)
+dtm.positive.df.freq.data <- data.frame(word = names(dtm.positive.df), freq = dtm.positive.df) # shows them as a DF
+dtm.positive.df.freq.data
+
+
+
+
+
+
+
+
+
+
+
+
 
 # see how often the words that are left occur
 freq.plot <- ggplot(dtm.positive.df.freq.data, aes(reorder(word, freq), freq)) + geom_col() + 
