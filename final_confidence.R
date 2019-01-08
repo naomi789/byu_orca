1aafead843a0c2036a7e5e807ece71a2c9e435ea
naomi789 <snjohnson789@gmail.com>
2019-01-07 22:52:11 -0500

    notes about interpretation

--- final_confidence.R
+++ final_confidence.R
@@ -1,6 +1,6 @@
 confidence.data <- read.csv("./../secret_byu_data/confidence/2019_01_06_partial.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
 # there are 49 columns above
-answers.data <- confidence.data[,18:31]
+answers.data <- confidence.data[,c(18:33)]
 # confidence.df <- as.data.frame(as.matrix(confidence.data))
 colnames(answers.data)
 answers.data[,8] <- as.numeric(answers.data[,8])
@@ -10,16 +10,37 @@ library(devtools)
 library(qgraph)
 # head(confidence.data[,18:31])
 
+
+answers.data <- answers.data[,c(-10,-11,-14)] # takes out binary data, strings
 str(answers.data)
-answers.data <- answers.data[,c(-10,-11, -14)] # takes out binary data, strings
 cor_confidence <- cor_auto(answers.data, ordinalLevelMax = 8)
+# can I set Q4.2 aka [12] to be ordinal as well??
 qgraph(cor_confidence, directed=FALSE, layout = "spring")
+# INTERPRETATION
+# things that make sense: 
+# Q2.1	Q2.2	Q2.3 (are all strongly positively correlated)
+# Q1.2 Q 1.1 are negatively correlated  
+# Q3.3 (major) and Q2.2 (career w/ programming)
+# Q4.3 (grades, but reversed) and Q3.2 (GPA)
+# Q4.2 and Q2.2 (double check this)
+
+
 
 centrality_confidence <- centrality(cor_confidence)
 # checkout Betweenness, etc
+centrality_confidence$Betweenness
 
 library(EGA)
 ega.trump.ggm <- EGA(data.trump, model = "glasso")
+
+
+
+# things that surprise me: 
+# Q3.2 (GPA) and Q2.1 ('My experience in my CS course(s) THIS PAST SEMESTER leads me to believe I would be successful in future computing activities.")
+# what does ^^ that relationship mean
+
+
+
 
 # library(NetworkToolbox)
 # scores.trump <- nams(data.trump, A = ega.trump.tmfg$network, comm = ega.trump.tmfg$wc,standardize = TRUE)
