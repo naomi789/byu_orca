7692ca820a3b47b223a043ef48134adb12242d1b
naomi789 <snjohnson789@gmail.com>
2019-01-08 20:10:49 -0500

    all weeks have six datapoints

--- longitudinal.R
+++ longitudinal.R
@@ -6,7 +6,7 @@ library(lavaan)
 # first week
 weeka <- read.csv("./../secret_byu_data/confidence/2018_09_26.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
 # weeka <- weeka[,c(2,12:13,19:28)]
-weeka <- weeka[,c(12, 25:28)]
+weeka <- weeka[,c(2, 12, 25:28)]
 
 # regular middle weeks
 weekb <- read.csv("./../secret_byu_data/confidence/2018_10_24.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
@@ -27,4 +27,4 @@ weekf <- weekf[,c(2,12,18:21)]
 # last week
 weekg <- read.csv("./../secret_byu_data/confidence/2019_01_06_partial.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
 # weekg <- weekg[,c(2,12,18:50)]
-weekg <- weekg[,c(12,23:26)]
+weekg <- weekg[,c(2, 12,23:26)]
