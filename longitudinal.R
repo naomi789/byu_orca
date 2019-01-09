226bd9d6a8f96c31e8b3d52b1f587910eb15322f
naomi789 <snjohnson789@gmail.com>
2019-01-08 21:39:30 -0500

    four graphs of four questions

--- longitudinal.R
+++ longitudinal.R
@@ -1,13 +1,14 @@
 # Latent Growth Curve Models (LGCM) # use lavaan (pretty straightforward) 
 # https://www.youtube.com/watch?v=vrHaAdjC97A
 library(lavaan)
-l
+library(ggplot2)
+library(data.table)
 
 # read in the data
 # first week
-weeka <- read.csv("./../secret_byu_data/confidence/2018_09_26.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
+weeka_original <- read.csv("./../secret_byu_data/confidence/2018_09_26.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
 # weeka <- weeka[,c(2,12:13,19:28)]
-weeka <- weeka[,c(2, 12, 25:28)]
+weeka <- weeka_original[,c(2, 12, 25:28)] # 24, 22 = major
 
 # regular middle weeks
 weekb <- read.csv("./../secret_byu_data/confidence/2018_10_24.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
@@ -26,9 +27,9 @@ weekf <- read.csv("./../secret_byu_data/confidence/2018_12_17.csv", header = TRU
 weekf <- weekf[,c(2,12,18:21)]
 
 # last week
-weekg <- read.csv("./../secret_byu_data/confidence/2019_01_06_partial.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
-# weekg <- weekg[,c(2,12,18:50)]
-weekg <- weekg[,c(2, 12,23:26)]
+weekg_original <- read.csv("./../secret_byu_data/confidence/2019_01_06_partial.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
+# weekg <- week_original[,c(2,12,18:50)]
+weekg <- weekg_original[,c(2,12,23:26)] # 18-22 # 27 = gender # 29 is major # 31 and 32 = course and grade (respectively)
 
 
 weeka$week <- 4 # 4
@@ -40,6 +41,14 @@ weekf$week <- 16 # 16
 weekg$week <- 19 # 19
 
 weekData <- rbind(weeka,weekb,weekc,weekd,weeke,weekf,weekg)
+weeka <- NULL
+weekb <- NULL
+weekc <- NULL
+weekd <- NULL
+weeke <- NULL
+weekf <- NULL
+weekg <- NULL
+
 
 colnames(weekData) <- c("EndDate","Email","FutureSuccess",
                         "PostGradCSPlans","BetterCSthanGE",
@@ -77,9 +86,48 @@ sPlot <- ggplot(weekData, aes(x=week, y=FutureSuccess, group=Email))+
 
 sPlot
 
+weekData.wide <- data.frame(dcast(setDT(weekData), Email ~ week, value.var = c("FutureSuccess","PostGradCSPlans",
+                                                                               "BetterCSthanGE","BetterCSthanGrades")))
+
+weekData.wide$MajorAtBegining <- NA 
+weekData.wide$MajorAtEnd <- NA
+weekData.wide$Gender <- NA 
+weekData.wide[which(weekData.wide$Email %in% weeka_original$RecipientEmail),"MajorAtBegining"] <- weeka_original[which(weekData.wide$Email %in% weeka_original$RecipientEmail),22]
+weekData.wide[which(weekData.wide$Email %in% weekg_original$RecipientEmail),"MajorAtEnd"] <- weekg_original[which(weekData.wide$Email %in% weekg_original$RecipientEmail),29]
+weekData.wide[which(weekData.wide$Email %in% weekg_original$RecipientEmail),"Gender"] <- weekg_original[which(weekData.wide$Email %in% weekg_original$RecipientEmail),27]
+
+matplot(c(4,8,10,12,15,16,19),
+        main="...successful in future computing activities",
+        t(weekData.wide[,2:8]),type="l",
+        col=ifelse(weekData.wide$Gender==1,"red2",
+                   ifelse(weekData.wide$Gender==2,"blue",NA)),
+        ylab="Agreement, 1-7",
+        xlab="Week")                                                           
+
+
+matplot(c(4,8,10,12,15,16,19),
+        main="...after grad... CS pursue a career that inv",
+        t(weekData.wide[,9:15]),type="l",
+        col=ifelse(weekData.wide$Gender==1,"red2",
+                   ifelse(weekData.wide$Gender==2,"blue",NA)),
+        ylab="Likeliness, 1-7",
+        xlab="Week") 
 
 
 
+matplot(c(4,8,10,12,15,16,19),
+        main="...better at CS courses than other courses...",
+        t(weekData.wide[,16:22]),type="l",
+        col=ifelse(weekData.wide$Gender==1,"red2",
+                   ifelse(weekData.wide$Gender==2,"blue",NA)),
+        ylab="Certainty, 1-5",
+        xlab="Week") 
 
 
-
+matplot(c(4,8,10,12,15,16,19),
+        main="...better at CS than CS GRADES",
+        t(weekData.wide[,23:29]),type="l",
+        col=ifelse(weekData.wide$Gender==1,"red2",
+                   ifelse(weekData.wide$Gender==2,"blue",NA)),
+        ylab="Agreement, 1-7",
+        xlab="Week") 
