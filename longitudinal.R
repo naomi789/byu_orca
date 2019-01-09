5c912897f0adcf7c0bdd44670fb8b4f5878de32e
naomi789 <snjohnson789@gmail.com>
2019-01-08 20:46:26 -0500

    fix x axis

--- longitudinal.R
+++ longitudinal.R
@@ -1,6 +1,7 @@
 # Latent Growth Curve Models (LGCM) # use lavaan (pretty straightforward) 
 # https://www.youtube.com/watch?v=vrHaAdjC97A
 library(lavaan)
+l
 
 # read in the data
 # first week
@@ -28,3 +29,57 @@ weekf <- weekf[,c(2,12,18:21)]
 weekg <- read.csv("./../secret_byu_data/confidence/2019_01_06_partial.csv", header = TRUE, stringsAsFactors = FALSE,na.strings = "")
 # weekg <- weekg[,c(2,12,18:50)]
 weekg <- weekg[,c(2, 12,23:26)]
+
+
+weeka$week <- 4 # 4
+weekb$week <- 8 # 8
+weekc$week <- 10 # 10
+weekd$week <- 12 # 12
+weeke$week <- 15 # 15
+weekf$week <- 16 # 16
+weekg$week <- 19 # 19
+
+weekData <- rbind(weeka,weekb,weekc,weekd,weeke,weekf,weekg)
+
+colnames(weekData) <- c("EndDate","Email","FutureSuccess",
+                        "PostGradCSPlans","BetterCSthanGE",
+                        "BetterCSthanGrades","week")
+
+weekData[weekData=="Strongly disagree"] <- 1
+weekData[weekData=="Somewhat disagree"] <- 2
+weekData[weekData=="Disagree"] <- 3
+weekData[weekData=="Neither agree nor disagree"] <- 4
+weekData[weekData=="Agree"] <- 5
+weekData[weekData=="Somewhat agree"] <- 6
+weekData[weekData=="Strongly agree"] <- 7
+
+weekData[weekData=="Extremely unlikely"] <- 1
+weekData[weekData=="Moderately unlikely"] <- 2
+weekData[weekData=="Slightly unlikely"] <- 3
+weekData[weekData=="Neither likely nor unlikely"] <- 4
+weekData[weekData=="Slightlylikely"] <- 5
+weekData[weekData=="Moderately likely"] <- 6
+weekData[weekData=="Extremely likely"] <- 7
+
+weekData[weekData=="Definitely not"] <- 1
+weekData[weekData=="Probably not"] <- 2
+weekData[weekData=="Might or might not"] <- 3
+weekData[weekData=="Probably yes"] <- 4
+weekData[weekData=="Definitely yes"] <- 5
+
+weekData[,c("FutureSuccess","PostGradCSPlans",
+            "BetterCSthanGE","BetterCSthanGrades")] <- apply(weekData[,c("FutureSuccess","PostGradCSPlans",
+                                                                         "BetterCSthanGE","BetterCSthanGrades")],
+                                                             2,
+                                                             function(x) as.numeric(x))
+sPlot <- ggplot(weekData, aes(x=week, y=FutureSuccess, group=Email))+
+  geom_line()
+
+sPlot
+
+
+
+
+
+
+
